dabd9348a9fdfa52cbeaf97631f81a7a5baf6813
Maverickspb78 <Maverickspb78@gmail.com>
2022-08-30 13:45:39 +0300

    Initial commit

---
+++ src/main/java/com/example/markerovkaoasis/MarkerovkaOasisApplication.java
@@ -0,0 +1,13 @@
+package com.example.markerovkaoasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkerovkaOasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkerovkaOasisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/markerovkaoasis/MarkerovkaOasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.markerovkaoasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkerovkaOasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
